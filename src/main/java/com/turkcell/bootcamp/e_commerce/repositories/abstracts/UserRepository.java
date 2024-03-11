1cf5e40b810fbf8dc04ac94947308e35f43013ec
Erdem Baran <erdmbrn13@gmail.com>
2024-03-11 17:07:35 +0300

    user repo commit

--- src/main/java/com/turkcell/bootcamp/e_commerce/repositories/abstracts/UserRepository.java
+++ src/main/java/com/turkcell/bootcamp/e_commerce/repositories/abstracts/UserRepository.java
@@ -2,13 +2,12 @@ package com.turkcell.bootcamp.e_commerce.repositories.abstracts;
 
 import com.turkcell.bootcamp.e_commerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    void add(User user);
-    void delete(int id);
-    void update(User user);
-    List<User> getAll();
-    User getById(int id);
+    @Query("SELECT COUNT(u) FROM User u")
+    int countAllUsers();
+
 }
